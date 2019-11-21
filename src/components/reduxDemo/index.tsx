b627c4335281eaf900f22e4a23df211b5c92a98e
dandan <18856856168@163.com>
2019-11-21 23:15:15 +0800

    添加 redux-actions Demo

--- src/components/reduxDemo/index.tsx
+++ src/components/reduxDemo/index.tsx
@@ -4,14 +4,46 @@ import { createActions } from 'redux-actions'
 
 const { increment, decrement } = createActions('INCREMENT', 'DECREMENT')
 
-class reduxDemo extends Component {
+interface reduxDemoProps {
+  counter: number
+  increment: Function
+  decrement: Function
+}
+
+class reduxDemo extends Component<reduxDemoProps> {
+  constructor(props: reduxDemoProps) {
+    super(props)
+  }
   render() {
-    // const { counter } = this.props
+    console.log(this.props)
     return (
       <div>
         <div>reduxDemo</div>
         <div>{JSON.stringify(this.props)}</div>
-        {/* <div>{this.props.counter}</div> */}
+        <span
+          onClick={() => this.props.increment(1)}
+          style={{
+            fontSize: '25px',
+            color: 'red',
+            cursor: 'pointer',
+            fontWeight: 700,
+            userSelect: 'none'
+          }}
+        >
+          +
+        </span>
+        <span
+          onClick={() => this.props.decrement(1)}
+          style={{
+            fontSize: '25px',
+            color: 'red',
+            cursor: 'pointer',
+            fontWeight: 700,
+            userSelect: 'none'
+          }}
+        >
+          -
+        </span>
       </div>
     )
   }
@@ -19,16 +51,15 @@ class reduxDemo extends Component {
 
 const mapStateToProps = (state: any) => {
   return {
-    counter: state.counter
+    counter: state.add.counter
   }
 }
 
 const matchDdipatchToProps = (dispatch: any) => {
   return {
-    increment: () => dispatch(increment()),
-    decrement: () => dispatch(decrement())
+    increment: (value: number) => dispatch(increment(value)),
+    decrement: (value: number) => dispatch(decrement(value))
   }
 }
 
 export default connect(mapStateToProps, matchDdipatchToProps)(reduxDemo)
-// export default reduxDemo
--- src/reducers/demoReducer.tsx
+++ src/reducers/demoReducer.tsx
@@ -1,21 +1,12 @@
 import { handleActions } from 'redux-actions'
 
-// export INCREMENT
-// export DECREMENT
-
-// const add = handleAction(
-//   'ADD',
-//   (state: any, action: any) => state + 2,
-//   0
-// )
-
 const add = handleActions(
   {
-    'INCREMENT': (state, action: any) => ({
+    INCREMENT: (state, action: any) => ({
       counter: state.counter + action.payload
     }),
 
-    'DECREMENT': (state, action: any) => ({
+    DECREMENT: (state, action: any) => ({
       counter: state.counter - action.payload
     })
   },
